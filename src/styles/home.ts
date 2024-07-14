05a42bd216ac76ad1506cc44fa94c9bd8515dfe3
Phuoc <khuathoduyphuoc2001@gmail.com>
2024-07-15 00:37:38 +0700

    Doing responsive introduce

--- src/styles/home.ts
+++ src/styles/home.ts
@@ -21,9 +21,9 @@ export const themes: I_Style_Theme = {
 // mdn: min-width: 1200px
 export const introduce: I_Style_Introduce = {
   parentItem: 'mdn:flex justify-center items-center md:grid md:grid-cols-2 relative',
-  leftText: 'col-span-1 absolute top-10 mdn:left-44 left-0 text-4xl font-bold w-64',
-  imgItem: 'col-span-1 col-start-2 md:opacity-1 opacity-0',
-  rightText: 'col-span-1 absolute top-50 bottom-10 mdn:right-0 w-96',
+  leftText: 'col-span-1 absolute top-10 mdn:left-44 md:left-0 text-4xl font-bold w-64',
+  imgItem: 'col-span-1 col-start-2 md:block hidden',
+  rightText: 'col-span-1 absolute top-52 bottom-10 mdn:right-0 w-96',
   thirdChildText: 'text-sm font-medium',
   avatarSize: 'w-[42.8rem] h-[30.8rem]'
 }
